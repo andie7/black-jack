a1b499d3935fe994095aee260db3f2d8e68c867d
andie7 <131781648+andie7@users.noreply.github.com>
2024-03-12 15:32:30 +0100

    App finalized with Js and CSS

--- script.js
+++ script.js
@@ -3,21 +3,16 @@
 // Set their values to a random number between 2 and 11
 
 //Create a variable sum and set it to the sum of the two cards
+let cards = [];
 
-let firstCard = getRandomCard();
-
-let secondCard = getRandomCard();
-
-let cards = [firstCard, secondCard]; // array order a list of items
-
-let sum = firstCard + secondCard;
+let sum = 0;
 // Create a variable hasBlackJack and set it to false
 
 let hasBlackJack = false;
 
 // Create a variable isAlive and set it to true
 
-let isAlive = true;
+let isAlive = false;
 
 // Declare a variable called message and assign its value to an empty string and reassign the message variable to the string we're logging out
 
@@ -39,15 +34,54 @@ console.log(sumEl);
 
 cardsEl = document.querySelector("#cards-el");
 
+//Player - create an Object / The syntax in an object is different. We use : after the key and use colon to separate the var. The first is the key and second is the value
+//The difference between arrays and objects is that the
+
+let player = {
+  Name: "Andreia",
+
+  Chips: 150,
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.Name + ": €" + player.Chips;
+
 // Cash Out
 console.log(hasBlackJack);
 console.log(isAlive);
 
 //Log it out
 
+//  Create a function getRandomCard that always returns a number between 1 and 13
+
+function getRandomCard() {
+  // if 1 -> return 11
+  // if  11-13  return 10
+
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+  if (randomCard > 10) {
+    return 10;
+  } else if (randomCard === 1) {
+    return 11;
+  } else {
+    return randomCard;
+  }
+}
+
 //Call a new function called startGame that calls renderGame()
 
 function startGame() {
+  isAlive = true;
+  // Generate two random numbers
+  //Re-assign the cards and sum variables so that the game can start
+  let firstCard = getRandomCard();
+
+  let secondCard = getRandomCard();
+
+  cards = [firstCard, secondCard]; // array order a list of items
+
+  sum = firstCard + secondCard;
+
   renderGame();
 }
 // function startGame
@@ -78,15 +112,18 @@ function renderGame() {
 function newCard() {
   console.log("Drawing a new card from the deck");
 
-  // Create a new card variable and hard code to a number
-  let card = getRandomCard();
-  // Add the new card to the sum variable
-  sum += card;
+  // Only allow the player to get a new card if she is alive and does not have black jack
+  if (isAlive === true && hasBlackJack === false) {
+    // Create a new card variable and hard code to a number
+    let card = getRandomCard();
+    // Add the new card to the sum variable
+    sum += card;
 
-  // push the card to the cards array
-  cards.push(card);
-  //Call startGame()
-  renderGame();
+    // push the card to the cards array
+    cards.push(card);
+    //Call startGame()
+    renderGame();
+  }
 }
 //Exercise
 // Check if the person is old enough to enter the nightclub (21)
@@ -148,3 +185,83 @@ for (let i = 0; i < carts.length; i += 1) {
 }
 
 // greetingEL.textcontext += sentence[i]
+
+// Generate random numbers
+// Math.round generate a random number between 0 and 1
+//  Math.round *6 gives us  a random number between 0 and 6
+
+let randomNumber = Math.random() * 6;
+
+console.log(randomNumber);
+
+// Math.floor removes the decimals
+
+let flooredNumber = Math.floor(3.4638);
+console.log(flooredNumber);
+
+let randomNum = Math.floor(Math.random() * 6);
+
+let randomNu = Math.floor(Math.random() * 6) + 1;
+
+function rollDice() {
+  let randomNum = Math.floor(Math.random() * 6) + 1;
+  return randomNum;
+}
+
+console.log(rollDice());
+
+// just want to call the function if two conditions are true
+
+let hasCompletedCourse = true;
+let giveCertificate = true;
+
+if (hasCompletedCourse === true && giveCertificate === true) {
+  generateCertificate();
+}
+
+function generateCertificate() {
+  console.log("Generating Certificate");
+}
+
+// Create an if statement that checks that both variables are false
+
+// if so, run the showSolution function
+
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
+
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution();
+}
+
+function showSolution() {
+  console.log("Showing the solution");
+}
+
+//OR operators
+
+//Create a bollean variable likesDocumentaries and likesStartups
+// use an OR statement to call recommendMovies() if either conditions are true
+
+let likesDocumentaries = true;
+
+let likesStartups = false;
+
+if (likesDocumentaries === true || likesStartups === true) {
+  recommendMovies();
+}
+
+function recommendMovies() {
+  console.log("Recommend Movies");
+}
+
+// Object
+
+let castle = {
+  title: "Live like a King",
+  price: 190,
+  isSuperHost: true,
+  images: "#",
+};
+
+console.log(castle.price);
